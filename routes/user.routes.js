4f44e8e216391de03cecf247e6ec74a954b23091
Pradeep Lama <tamangjake@gmail.com>
2025-02-12 22:01:22 +0545

    feat: added authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middleware/auth.middleware.js";
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.send("Welcome to the Subscription Tracker API!");
-});
+userRouter.get("/", getUsers);
+userRouter.get("/:id", authorize, getUser);
 
 export default userRouter;
